7d135c73325e131654e57424ba64a3e681e17882
LiamMcMainsBR <118222629+LiamMcMainsBR@users.noreply.github.com>
2023-07-11 15:49:18 -0500

    Add basic UI

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,62 @@ void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  int index = 0;
+  bool isAdmin = false;
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      theme: ThemeData(useMaterial3: true),
       home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Deferred Components'),
+          actions: [
+            IconButton(onPressed: () {}, icon: const Icon(Icons.person)),
+            if (isAdmin)
+              IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.admin_panel_settings)),
+          ],
+        ),
         body: Center(
-          child: Text('Hello World!'),
+          child: Column(children: [
+            FilledButton.tonal(
+              child: const Text('Become admin'),
+              onPressed: () {
+                setState(() {
+                  isAdmin = true;
+                });
+              },
+            ),
+            FilledButton.tonal(
+              child: const Text('Remove admin'),
+              onPressed: () {
+                setState(() {
+                  isAdmin = false;
+                });
+              },
+            )
+          ]),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: index,
+          onTap: (value) => setState(() {
+            index = value;
+          }),
+          items: const [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.dashboard), label: "Dashboard"),
+            BottomNavigationBarItem(icon: Icon(Icons.warning), label: "Issues")
+          ],
         ),
       ),
     );
